2eb0209501dad76b3b4b6e485cf92273d5d2dace
WisonyTQ <wisonytq@missionreadyhq.com>
2025-06-27 16:15:10 +1200

    added stations detail page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Footer from "./components/Footer";
-import Header from './components/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Map from "./components/Map";
 import Search from "./components/Search";
 import SearchRoute from "/routes/SearchRoute";
+import StationDetails from "./components/StationDetails";
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen">
-        <Routes>
-        </Routes>
         <Header /> {/* ✅ Drop your header here */}
         <Search></Search>
         <Map />
+        <Routes>
+          <Route path="/station/:id" element={<StationDetails />} />
+        </Routes>
         <Footer /> {/* ✅ Drop your footer here */}
       </div>
     </Router>
   );
 }
- 
+
 export default App;
